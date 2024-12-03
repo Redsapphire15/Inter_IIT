0f6846ce5b0b7cf0b06b7186d5172e2dcf5309c7
Abhay Gokhale <abhay.u.gokhale@gmail.com>
2024-12-03 13:25:46 +0530

    Update global_database_node.py
    Fixed a small bug
    
    
    Signed-off-by: Abhay Gokhale <abhay.u.gokhale@gmail.com>

--- scripts/global_database_node.py
+++ scripts/global_database_node.py
@@ -151,7 +151,7 @@ class GlobalDatabaseNode(Node): # Creates a node that acts as the interface for 
 
     def assign_callback(self,selected_robot,selected_target):
         try:
-            # Load existing data from JSON file
+            self.assign_file = open(self.assign_target,"r+")
             self.assign_file.seek(0)  # Move to the beginning
             existing_data = json.load(self.assign_file)
 
@@ -184,11 +184,32 @@ class GlobalDatabaseNode(Node): # Creates a node that acts as the interface for 
         pygame.draw.line(self.screen, color, p1, p2, 5)
         pygame.draw.line(self.screen, color, p3, p4, 5)
 
-    def render_tasks(self):               # Render the tasks list
-        task_color = pygame.Color(255, 0, 0)
-        for _, location in self.tasks.items():
+    def render_tasks(self):
+        self.assign_file = open(self.assign_target,"r")
+        task_color = pygame.Color(255, 0, 0)  # Default color for targets
+        assigned_color = pygame.Color(0, 0, 255)  # Color for assigned targets
+        try:
+            # Load existing data from JSON file
+            self.assign_file.seek(0)  # Move to the beginning
+            existing_data = json.load(self.assign_file)
+
+            for task_entry in existing_data["target"]:
+                print(task_entry["object_id"],task_entry["is_assigned"])
+                if task_entry["is_assigned"] == True:
+                    pygame.draw.circle(self.screen, assigned_color, (int(task_entry["coords"][0]*self.scale) , self.screen.get_height() - int(task_entry["coords"][1]*self.scale)), 35, 3)
+
+        except Exception as e:
+            self.get_logger().error(f"Error updating: {e}")
+
+
+        for task_name, location in self.tasks.items():
             position = [int(coord) for coord in location]
             self.draw_cross(position, task_color)
+            font = pygame.font.Font(None, 24)
+            info_text = f"{task_name}"
+            text_surface = font.render(info_text, True, (0, 0, 0))
+            self.screen.blit(text_surface, (position[0] + 40, position[1] - 10))
+
 
     def render_robot(self, robot_id):     # Draw robot and path taken by the robot
         position = self.robot_data[robot_id]["Position"]
@@ -246,6 +267,7 @@ class GlobalDatabaseNode(Node): # Creates a node that acts as the interface for 
             self.tasks[i["name"]] = (int(i["x"]*self.scale),1000 - int(i["y"]*self.scale))
 
     def run_pygame(self):
+        selected_robot = False
         while rclpy.ok() and self.CL:
             rclpy.spin_once(self, timeout_sec=0.1)
 
@@ -260,6 +282,7 @@ class GlobalDatabaseNode(Node): # Creates a node that acts as the interface for 
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     mouse_pos = pygame.mouse.get_pos()
                     if event.button == 1:
+                        print("Hi!")
                         for i in range(1,self.num_robots + 1):
                             bot_pos = self.robot_data[i]["Position"]
                             if np.sqrt((mouse_pos[0]-bot_pos[0])**2 + (mouse_pos[1]-bot_pos[1])**2) <= 25:
@@ -268,13 +291,16 @@ class GlobalDatabaseNode(Node): # Creates a node that acts as the interface for 
                                 break
 
                         if selected_robot and not selected_target:
+                            print("Ola!")
                             for task_name, location in self.tasks.items():
                                 if np.sqrt((mouse_pos[0] - location[0])**2 + (mouse_pos[1] - location[1])**2) <= 25:
                                     selected_target = task_name
                                     break
                         if selected_robot and selected_target:
+                            print("Pepe")
                             self.assign_list[selected_robot] = selected_target
                             self.assign_callback(selected_robot,selected_target)
+                            selected_robot = False
 
             self.update_data()
             for i in range(1, self.num_robots + 1):
@@ -288,6 +314,7 @@ class GlobalDatabaseNode(Node): # Creates a node that acts as the interface for 
 
         pygame.quit()
         self.assign_file.close()
+
     def update_pygame(self):    # Update loop
         self.screen.fill((255,255,255))
         if hasattr(self, 'surf'):         # Check if map is loaded
